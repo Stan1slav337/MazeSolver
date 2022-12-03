8b6d488bd1e1b2068665afa51a2dc1d024b198db
Stas <stasbrega@inbox.ru>
2022-12-03 17:32:36 +0200

    add paintEvent (default grid shown)

--- MazeSolver.cpp
+++ MazeSolver.cpp
@@ -4,4 +4,11 @@ MazeSolver::MazeSolver(QWidget *parent): QMainWindow(parent)
 {
     setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT + MENU_HEIGHT));
     ui.setupUi(this);
+
+    alg = new SearchAlgo(this);
+}
+
+void MazeSolver::paintEvent(QPaintEvent* event)
+{
+    alg->showMaze();
 }
--- MazeSolver.h
+++ MazeSolver.h
@@ -1,13 +1,16 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QPainter>
 #include "ui_MazeSolver.h"
+#include "SearchAlgo.h"
 
 constexpr auto MENU_HEIGHT = 25;
 constexpr auto WINDOW_WIDTH = 500;
 constexpr auto WINDOW_HEIGHT = 500;
-constexpr auto DEFAULT_N = 100;
+constexpr auto DEFAULT_N = 10;
 constexpr auto BLOCK_COLOR = Qt::darkCyan;
+constexpr auto VISITED_BLOCK = Qt::cyan;
 
 class MazeSolver : public QMainWindow
 {
@@ -18,4 +21,7 @@ public:
 
 private:
     Ui::MazeSolverClass ui;
+    class SearchAlgo* alg;
+
+    virtual void paintEvent(QPaintEvent* event);
 };
