8a209f35ec5201e921cb86483e3c918e985947c4
Stas <stasbrega@inbox.ru>
2022-12-04 17:20:46 +0200

    add random maze generator

--- Maze.cpp
+++ Maze.cpp
@@ -1,17 +1,65 @@
 #include "Maze.h"
 
-Maze::Maze()
+Maze::Maze(const int LEN)
 {
-	binaryGrid = {
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1, 1, 0, 1, 0, 1},
-		{1, 0, 1, 0, 0, 1, 0, 0, 0, 1},
-		{1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	};
+	N = LEN;
+
+	generateBinaryGrid();
+}
+
+void Maze::generateBinaryGrid()
+{
+	binaryGrid.resize(N, std::vector<bool>(N, 1));
+
+	binaryGrid[1][1] = 0;
+
+	std::queue<Utils::point> nodes;
+	nodes.push({ 1, 1 });
+
+	while (!nodes.empty())
+	{
+		auto [cordX, cordY] = nodes.front(); nodes.pop();
+
+		std::map<const Utils::direction, int> maxMoves = {
+			{Utils::LEFT,  cordX - 1    },
+			{Utils::RIGHT, N - cordX - 2},
+			{Utils::UP,    cordY - 1    },
+			{Utils::DOWN,  N - cordY - 2}
+		};
+
+		for (auto [dir, dirCords] : Utils::moves)
+		{
+			auto [dirX, dirY] = dirCords;
+			int toMove = Utils::getRandom(0, maxMoves[dir]);
+
+			int nextX = cordX, nextY = cordY;
+			for (int i = 0; i < toMove; ++i)
+			{
+				if (binaryGrid[nextX + dirX][nextY + dirY] == 0)
+					break;
+
+				bool ok = true;
+				for (auto [nextDir, nextDirCords] : Utils::moves)
+				{
+					auto [nextDirX, nextDirY] = nextDirCords;
+
+					if (dirX == -nextDirX && dirY == -nextDirY)
+						continue;
+
+					if (binaryGrid[nextX + dirX + nextDirX][nextY + dirY + nextDirY] == 0)
+						ok = false;
+				}
+
+				if (!ok)
+					break;
+
+				nextX += dirX, nextY += dirY;
+				binaryGrid[nextX][nextY] = 0;
+			}
+
+			if (nextX != cordX || nextY != cordY)
+				nodes.push({ nextX, nextY });
+		}
+	}
+
 }
--- Maze.h
+++ Maze.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#include <vector>
 #include "SearchAlgo.h"
 #include "GridBlock.h"
 
 class Maze
 {
 public:
-	Maze();
+	Maze(const int);
 
 private:
 	friend class SearchAlgo;
+
+	void generateBinaryGrid();
+
+	int N;
 	std::vector<GridBlock> grid;
 	std::vector<std::vector<bool>> binaryGrid;
 };
